80d13da81c63e9a839ef79278defa927301cfe18
Chinmay Anand <chinmayanand896@gmail.com>
2019-11-26 01:06:01 +0530

    initial commit to master

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="flex justify-center p-6">
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import './assets/css/main.css';
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,8 +18,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
