470b451645f43092c823891104b8c6e235c84545
Chinmay Anand <chinmayanand896@gmail.com>
2019-12-04 01:51:41 +0530

    added and tested sweet alert

--- src/components/AddChannel.vue
+++ src/components/AddChannel.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="modal-fade">
+  <transition name="fademodal">
     <div class="modal-backdrop" @keyup.esc="close">
       <div class="flex-col flex bg-white text-gray-800 text-sm rounded-lg p-6 w-full max-w-md">
         <header class="flex items-center justify-center p-3">
@@ -7,16 +7,24 @@
               Add a new Channel
           </p>
         </header>
-        <input type="text" class="border border-gray-300 p-2 rounded w-full 
+        <form>
+            <input v-model="new_channel" type="text" class="border border-gray-300 p-2 rounded w-full 
             focus:outline-none hover:border-gray-500 text-lg" 
             placeholder="Enter channel name...">
-        <footer class="flex items-center justify-between mt-5">
-            <button class="block p-2 text-sm rounded bg-blue-400 font-semibold text-white hover:bg-blue-600">
+            <ul v-if="hasErrors">
+                <li class="text-red-800 text-xs font-semibold" v-for="error in errors" :key="error.id">{{ error }}</li>
+            </ul>
+            <sweetalert-icon v-if="channelAddedSuccesfully" icon="success"></sweetalert-icon>
+            <sweetalert-icon v-if="addingChannel" icon="loading"></sweetalert-icon>
+        </form>
+        <footer class="flex items-center justify-between mt-12">
+            <button @click="add_new_channel" class="block p-2 text-sm rounded bg-blue-400 font-semibold text-white hover:bg-blue-600">
                 Add new Channel
             </button>
             <button @click="close" class="block p-2 text-sm rounded bg-red-400 font-semibold text-white hover:bg-red-600">
                 Cancel
             </button>
+
         </footer>
       </div>
     </div>
@@ -24,11 +32,59 @@
 </template>
 
 <script>
+import firebase from 'firebase/app';
+import SweetalertIcon from 'vue-sweetalert-icons';
   export default {
     name: 'addchannel',
+    components: { SweetalertIcon },
+    data(){
+        return{
+            new_channel: '',
+            errors: [],
+            channelsRef: firebase.database().ref('channels'),
+            channelAddedSuccesfully: false,
+            addingChannel: false,
+        }
+    },
+    computed: {
+        hasErrors(){
+            return this.errors.length > 0;
+        }
+    },
     methods: {
       close() {
+        this.errors = [];
         this.$emit('close');
+      },
+      add_new_channel(){
+          this.addingChannel = true;
+          let key = this.channelsRef.push().key;
+          let new_channel = {
+              id: key,
+              name: this.new_channel,
+          }
+          this.channelsRef.child(key).update(new_channel)
+            .then(() => {
+                this.addingChannel = false;
+                this.new_channel = '',
+                this.show_success_sweet_alert();
+                setTimeout(() => {
+                    this.close();
+                }, 3000);
+            })
+            .catch(error => {
+                this.new_channel = '',
+                this.errors.push(error.message);
+            })
+      },
+      show_success_sweet_alert(){
+          this.channelAddedSuccesfully = true;
+          setTimeout(() => {
+                this.hide_success_sweet_alert(); 
+            }, 5000);
+      },
+      hide_success_sweet_alert(){
+          this.channelAddedSuccesfully = false;
       },
     },
   };
@@ -46,4 +102,10 @@
     justify-content: center;
     align-items: center;
   }
+    .fademodal-enter-active, .fademodal-leave-active {
+    transition: opacity .5s;
+    }
+    .fademodal-enter, .fademodal-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    }
 </style>
